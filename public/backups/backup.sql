2d8740e944713a31e3ca79296986fca72192d002
mrpraj7488 <mr.praj7488@gmail.com>
2025-08-19 21:17:11 +0530

    Delete public directory

--- public/backups/backup.sql
+++
@@ -1,7 +0,0 @@
--- Indexes for profiles
-CREATE INDEX IF NOT EXISTS "idx_profiles_email" ON "profiles" ("email");
-CREATE INDEX IF NOT EXISTS "idx_profiles_referral_code" ON "profiles" ("referral_code");
-CREATE INDEX IF NOT EXISTS "idx_profiles_username" ON "profiles" ("username");
-CREATE INDEX IF NOT EXISTS "profiles_email_key" ON "profiles" ("email");
-CREATE INDEX IF NOT EXISTS "profiles_referral_code_key" ON "profiles" ("referral_code");
-CREATE INDEX IF NOT EXISTS "profiles_username_key" ON "profiles" ("username");
